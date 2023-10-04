0887a99931a654a276175cf830d5b0dca96fbb04
Karel Trisnanto Utomo <kareltrisnanto26@gmail.com>
2023-10-04 21:57:53 +0700

    add: initial files

---
+++ README.md
@@ -0,0 +1,50 @@
+# PROJECT MIDTERM EXAM WEB PROGRAMMING 1
+## STATIC WEBSITE TOURISM THEME
+
+### Name : Karel Trisnanto Utomo
+### NIM : 22090120
+### Class : 3C
+### Link URL Website : https://karmint26.github.io/go-trav
+
+##
+
+### OVERVIEW LANDING PAGE WEBSITE
+
+### 1. Light Mode
+<img src="./src/assets/Landing-GoTrav.png" width="100%" height="300">
+
+### 2. Dark Mode
+<img src="./src/assets/Landing-GoTrav-Dark.png" width="100%" height="300">
+
+### TECHNOLOGY STACK
+1. **HTML5**
+    - HTML5 is used to set the framework of the website that will be used.
+2. **CSS3**
+    - CSS3 is used to organize the appearance of the website so that it looks attractive.
+3. **JavaScript**
+    - JavaScript is used to set the behavior of the website to be more interactive and manipulate the DOM in it
+
+##
+
+### PROJECT STRUCTURE
+1. **src**
+    - **assets** = the assets folder is used to store images that will be used on the website.
+    - **css** = The css folder is used to store style.css files to organize the appearance of the website and also to make the structure of this website project better and more structured.
+    - **js** = This js folder is used to store the index.js file as a regulator of website behavior to make it more interactive and the folder structure in this project becomes tidier.
+2. **index.html** = This is the main file in the website that becomes the endpoint of the initial display of the website when it is opened.
+3. **README.md** = This is a documentation file regarding the description of this website project.
+
+### FEATURES AND ADVANTAGES
+1. **Responsive and Modern UI** = The GoTrav website already has a responsive and modern look for the entire website design. Responsive display is a display that looks good on any device such as mobile, tablet, or desktop.
+
+2. **Interactive Website** = The GoTrav website is already interactive with the help of javascript code in it to manipulate the DOM or Document Object Model and also the BOM or Browser Object Model.
+
+3. **Dark Mode** = The GoTrav website already supports dark mode and light mode themes, if the user uses a theme based on the os system, the website will automatically detect and display according to the theme selected on the os system, you can also change it manually by pressing the sun or moon button.
+
+4. **Code Documentation** = The GoTrav website already has good code documentation in it and the github repository also has good README.md documentation.
+
+5. **Comfortable Look** = The GoTrav website is very pleasing to the eye because of the right color selection and contrast. Users do not need to worry about being uncomfortable when viewing and visiting the GoTrav website.
+
+6. **Image Preview and Meta Open Graph** = GoTrav website has used image preview and meta open graph as SEO Friendly, when users share the website link, it will automatically display the image preview.
+
+7. **Loading Screen** = The GoTrav website has implemented a loading screen, meaning that when all images on the website have been downloaded by the user, the loading screen will disappear, but if you are still downloading, the loading screen is still running.
---
+++ src/js/index.js
@@ -0,0 +1,107 @@
+// Fitur Darkmode
+const htmlTag = document.querySelector("html");
+const toggleDarkMode = document.getElementById("dark-toggle");
+let count = 1;
+
+toggleDarkMode.addEventListener("click", () => {
+  htmlTag.classList.toggle("dark");
+  if (count % 2 == 1) {
+    toggleDarkMode.setAttribute("class", "bx bxs-moon");
+    count++;
+  } else {
+    toggleDarkMode.setAttribute("class", "bx bxs-sun");
+    count--;
+  }
+});
+
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  document.documentElement.classList.add("dark");
+  if (count % 2 == 1) {
+    toggleDarkMode.setAttribute("class", "bx bxs-moon");
+    count++;
+  } else {
+    toggleDarkMode.setAttribute("class", "bx bxs-sun");
+    count--;
+  }
+} else {
+  document.documentElement.classList.remove("dark");
+}
+
+// Whenever the user explicitly chooses light mode
+localStorage.theme = "light";
+
+// Whenever the user explicitly chooses dark mode
+localStorage.theme = "dark";
+
+// Whenever the user explicitly chooses to respect the OS preference
+localStorage.removeItem("theme");
+
+const githubBtn = document.querySelectorAll(".github");
+
+githubBtn.forEach((github) => {
+  github.addEventListener("click", () => {
+    window.open("https://github.com/KarMint26");
+  });
+});
+
+// Fitur Button back to top, ketika di click otomatis akan scroll ke paling atas
+const buttonBackTop = document.querySelector(".button-backtop");
+buttonBackTop.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+// Fitur Navigasi Bar ketika di scroll akan muncul shadow dibawahnya
+const navigationBar = document.querySelector(".navbar");
+
+function handleScroll() {
+  if (window.innerWidth >= 1024) {
+    if (window.scrollY > 30 || document.documentElement.scrollY > 30) {
+      navigationBar.style.boxShadow = "0 0.5px 6px 0 rgba(0, 0, 0, 0.2)";
+    } else {
+      navigationBar.style.boxShadow = "0 0.5px 6px 0 rgba(0, 0, 0, 0)";
+    }
+  }
+  if (window.scrollY > 30 || document.documentElement.scrollY > 30) {
+    buttonBackTop.style.display = "block";
+  } else {
+    buttonBackTop.style.display = "none";
+  }
+}
+
+window.addEventListener("scroll", handleScroll);
+
+window.addEventListener("resize", handleScroll);
+
+window.addEventListener("load", handleScroll);
+
+// Navbar Behavior
+const listNav = document.querySelectorAll(".list-nav");
+const dropdownMenu = document.querySelector(".dropdown");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const bodyOfHtml = document.querySelector("body");
+
+hamburgerMenu.addEventListener("click", () => {
+  dropdownMenu.classList.toggle("active");
+  hamburgerMenu.classList.toggle("active");
+  if (dropdownMenu.classList.contains("active")) {
+    bodyOfHtml.style.overflow = "hidden";
+  } else {
+    bodyOfHtml.style.overflow = "visible";
+  }
+});
+
+listNav.forEach((nav) => {
+  nav.addEventListener("click", () => {
+    dropdownMenu.classList.remove("active");
+    hamburgerMenu.classList.remove("active");
+    bodyOfHtml.style.overflow = "visible";
+  });
+});
