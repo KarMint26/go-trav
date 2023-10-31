980b8f9f99093363663ba61400004f1d52c06874
Karel Trisnanto Utomo <kareltrisnanto26@gmail.com>
2023-10-31 13:39:55 +0700

    add: PWA Features

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,13 @@
+// PWA Service Worker
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then((res) => console.log("service worker registered"))
+      .catch((err) => console.log("service worker not registered", err));
+  });
+}
+
 // Fitur Darkmode
 const htmlTag = document.querySelector("html");
 const toggleDarkMode = document.getElementById("dark-toggle");
